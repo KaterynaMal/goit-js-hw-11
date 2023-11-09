a625a966085eb5ddce5827abf0779da9a44fd858
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-09 12:33:59 +0000

    Deploying to gh-pages from @ KaterynaMal/goit-js-hw-11@dc4c72b2259b2322618354cc7361b68c717cd6d8 🚀

---
+++ index.9c6b38c5.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector('[name="searchQuery"]'),document.querySelector('[type="submit"]');
+//# sourceMappingURL=index.9c6b38c5.js.map
---
+++ index.d62be9c1.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form"),document.querySelector('[name="searchQuery"]'),document.querySelector('[type="submit"]');
+//# sourceMappingURL=index.d62be9c1.js.map
