dc4c72b2259b2322618354cc7361b68c717cd6d8
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-11-09 14:33:34 +0200

    + start files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+    searchQueryInput: document.querySelector('[name="searchQuery"]'),
+submitBtn: document.querySelector('[type="submit"]'),
+}
